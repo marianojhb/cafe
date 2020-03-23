91592770eebc0edfc6104539a0aeed46fe7019fb
marianojhb <mbelgrano@gmail.com>
2020-03-23 03:03:14 -0300

    added dynamic load with db in services, added active link color in nav bar

--- services/models.py
+++ services/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Services(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Titulo')
-    subtitle = models.CharField(max_length=200, verbose_name='Titulo')
-    content = models.CharField(max_length=200, verbose_name='Titulo')
-    image = models.ImageField(verbose_name='Imagen', upload_to='services')
-    created = models.DateTimeField(auto_now_add = True, verbose_name = "Fecha de creación")
-    updated = models.DateTimeField(auto_now_add = False, verbose_name = "Fecha de actualización")
+    title = models.CharField(max_length=200, verbose_name='Title')
+    subtitle = models.CharField(max_length=200, verbose_name='Subtitle')
+    content = models.CharField(max_length=200, verbose_name='Content')
+    image = models.ImageField(verbose_name='Image', upload_to='services', default="a")
+    created = models.DateTimeField(auto_now_add = True, verbose_name = "Created")
+    updated = models.DateTimeField(auto_now = True, verbose_name = "Updated")
 
     class Meta:
-        verbose_name = "Servicio"
-        verbose_name_plural = "Servicios"
+        verbose_name = "Service"
+        verbose_name_plural = "Services"
         ordering = ['-created']
     
     def __str__(self):
--- services/views.py
+++ services/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Services
 
 # Create your views here.
 def services(request):
-    return render(request, "services/services.html")
+    content = {
+        "services" : Services.objects.all()[::-1],
+    }
+    return render(request, "services/services.html", content)
