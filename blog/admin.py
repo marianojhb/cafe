85fabcdca036c0bf5dd90f72cb6e700e10fd52e9
marianojhb <mbelgrano@gmail.com>
2020-03-25 03:11:35 -0300

    Added blog models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from .models import Category, Post
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+    list_display = ("title", "author", "published")
+
+    # ordering para ordenar la tabla
+    ordering = ("author","published")
+    
+    # "author__username", "categories__name" son variables globales que se crean automaticamente
+    # search_fields muestra una lupa para buscar en estos campos
+    search_fields = ("title", "content", "author__username", "categories__name")
+
+
+    list_filter = ("author__username", "categories__name")
+
+    # Creamos un método para publicar
+    def post_categories(self, obj):
+        return ", ".join([c.name for c in obj.categories.all().order_by("name") ])
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Nombre")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Updated")
+
+    class Meta:
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
+        ordering = ["-created"]
+    
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    
+    # 'default=True' hace que 
+    published = models.DateTimeField(default=now, verbose_name="Fecha de publicación")
+    content = models.TextField(max_length=200, verbose_name="Contenido")
+    image = models.ImageField(verbose_name="Image", upload_to="blog")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Updated")
+    categories = models.ManyToManyField(Category, verbose_name="Categories", related_name="get_posts")
+    author = models.ForeignKey(User, verbose_name="Autor", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        ordering = ['-created']
+    
+    def __str__(self):
+        return self.title
